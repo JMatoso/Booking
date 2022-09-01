d200553f273ed5df5b0ee3ede7cbe710939cdb0f
José Matoso <jos3matosoj@gmail.com>
2022-09-01 12:05:01 +0100

    Cleaning

--- Booking/Metrics/MetricRegistry.cs
+++ Booking/Metrics/MetricRegistry.cs
@@ -5,28 +5,28 @@ namespace Booking.Metrics
 {
     public class MetricRegistry
     {
-        public static CounterOptions CreatedBooksCounter => new CounterOptions
+        public static CounterOptions CreatedBooksCounter => new()
         {
             Name = "Created Books",
             Context = "Booking.API",
             MeasurementUnit = Unit.Calls
         };
 
-        public static CounterOptions UpdatedBooksCounter => new CounterOptions
+        public static CounterOptions UpdatedBooksCounter => new()
         {
             Name = "Updated Books",
             Context = "Booking.API",
             MeasurementUnit = Unit.Calls
         };
 
-        public static CounterOptions DeletedBooksCounter => new CounterOptions
+        public static CounterOptions DeletedBooksCounter => new()
         {
             Name = "Deleted Books",
             Context = "Booking.API",
             MeasurementUnit = Unit.Calls
         };
 
-        public static CounterOptions CreatedDbContextCounter => new CounterOptions
+        public static CounterOptions CreatedDbContextCounter => new()
         {
             Name = "Created Database Context",
             Context = "Booking.API",
